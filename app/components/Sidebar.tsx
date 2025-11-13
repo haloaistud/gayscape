641da610799e46e71ef9613b5554fae7cf1cf057
Timothy <haloaistudios@gmail.com>
2025-11-12 21:12:51 -0600

    Create Sidebar.tsx

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const Sidebar: React.FC = () => (
+  <aside className="sidebar">
+    <ul>
+      <li>Home</li>
+      <li>Explore</li>
+      <li>Profile</li>
+    </ul>
+  </aside>
+);
+export default Sidebar;
