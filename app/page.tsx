1e01293383239476d205954b6cf35767f3147fca
Timothy <haloaistudios@gmail.com>
2025-11-12 21:05:10 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,31 @@
+"use client";
 import React, { useEffect, useState } from 'react';
 import Layout from './layout';
 import PostCard from './components/PostCard';
 import { fetchPosts } from './api';
 
+const REFRESH_INTERVAL = 60000;
+
 const HomePage: React.FC = () => {
   const [posts, setPosts] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const loadPosts = async () => {
+    setLoading(true);
+    const data = await fetchPosts();
+    setPosts(data);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    const loadPosts = async () => {
-      const data = await fetchPosts();
-      setPosts(data);
-    };
     loadPosts();
+    const interval = setInterval(loadPosts, REFRESH_INTERVAL);
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <Layout>
-      {posts.length ? (
-        posts.map((post) => <PostCard key={post.id} post={post} />)
-      ) : (
-        <p>Loading posts...</p>
-      )}
+      {loading ? <p className="loading">Loading feed...</p> : posts.map(post => <PostCard key={post.id} post={post} />)}
     </Layout>
   );
 };
